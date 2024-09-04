30114b6ac2d3b67c1d0845c4ab57061226ac1e8d
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-09-04 09:19:33 +0530

    React router dom added for dynamic routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Layout from './Layout'
+import { Home } from './pages'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route index element={<Home />} path='/' />
+
+    </Route >
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
