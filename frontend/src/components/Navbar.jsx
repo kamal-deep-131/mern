e849db81624f8ddbe0d2db2c3cd383c4e8022750
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-09-04 10:05:30 +0530

    Navbar Added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = () => {
+    const [login, setLogin] = useState(true)
+
     return (
-        <div>Navbar</div>
+        <header className='flex items-center justify-between p-2 md:py-2 md:px-4 shadow-md bg-white'>
+            <div className='flex items-center justify-center gap-2'>
+                <img src="./favicon.svg" className='w-6 md:w-8' alt="Bright side Logo" />
+                <span className='hidden md:inline font-bold text-xl'>Bright Side</span>
+            </div>
+            <div>
+                {login ?
+                    (
+                        <Link className='py-1 px-2 md:py-2 md:px-4 bg-primary text-sm md:text-base text-white font-medium rounded' to="/profile">Profile</Link>
+                    )
+                    : (
+                        <Link className='py-2 px-4 bg-primary text-white font-medium rounded' to="/login">Login</Link>
+                    )}
+            </div>
+        </header>
     )
 }
 
