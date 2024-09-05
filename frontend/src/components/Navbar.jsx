9bded84407561135b8dfb6ed3fb89a3ab82181e3
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-09-05 09:11:13 +0530

    Navbar style updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,18 +5,18 @@ const Navbar = () => {
     const [login, setLogin] = useState(true)
 
     return (
-        <header className='flex items-center justify-between p-2 md:py-2 md:px-4 shadow-md bg-white'>
+        <header className='flex items-center justify-between p-2 md:py-4 md:px-8 shadow-md bg-white'>
             <Link to={"/"} className='flex items-center justify-center gap-2'>
-                <img src="/favicon.svg" className='w-6 md:w-8' alt="Bright side Logo" />
-                <span className='hidden md:inline font-bold text-xl'>Bright Side</span>
+                <img src="/favicon.svg" className='w-6 md:w-10' alt="Bright side Logo" />
+                <span className='hidden md:inline font-bold text-2xl'>Bright Side</span>
             </Link>
             <div>
                 {login ?
                     (
-                        <Link className='py-1 px-2 md:py-2 md:px-4 bg-primary text-sm md:text-base text-white font-medium rounded' to="/profile">Profile</Link>
+                        <Link className='py-1 px-2 md:py-2 md:px-4 bg-primary text-sm md:text-lg text-white font-medium rounded' to="/profile">Profile</Link>
                     )
                     : (
-                        <Link className='py-2 px-4 bg-primary text-white font-medium rounded' to="/login">Login</Link>
+                        <Link className='py-1 px-2 md:py-2 md:px-4 bg-primary text-sm md:text-lg text-white font-medium rounded' to="/login">Login</Link>
                     )}
             </div>
         </header>
