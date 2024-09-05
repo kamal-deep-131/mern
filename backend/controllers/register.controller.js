c2105e2621823eeb287afcab7ef2e3fee67e08af
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-09-05 17:16:16 +0530

    register route with hasedpassword and token generation

--- backend/controllers/register.controller.js
+++ backend/controllers/register.controller.js
@@ -1,9 +1,53 @@
+import User from "../model/user.model.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 
-const register = (req, res) => {
+const register = async (req, res) => {
+    //get all fields
+    const { name, email, password } = req.body
 
-    res.json({
-        message: "Register"
+    //check all fields
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Please enter all fields"
+        })
+    }
+
+    //check if user already exists
+    const user = await User.findOne({ email })
+    if (user) {
+        return res.status(400).json({
+            success: false,
+            message: "User already exists"
+        })
+    }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    //create new user
+    const newUser = new User({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    await newUser.save()
+
+    //token genration
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET)
+
+    //response
+    res.status(201).json({
+        success: true,
+        message: "User created successfully",
+        token,
+        user: {
+            name: newUser.name,
+            email: newUser.email
+        }
     })
 }
 
