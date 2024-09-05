532579cc317ca0a32ed7887cf0a7a061087d55c9
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-09-05 09:47:21 +0530

    added form validation on all froms

--- frontend/src/pages/EditIdea.jsx
+++ frontend/src/pages/EditIdea.jsx
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputField, TextArea } from '../components'
+import { toast } from 'react-hot-toast'
 
 const EditIdea = () => {
+
+    const [formData, setFormData] = useState({
+        title: "",
+        description: "",
+    })
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (formData.title == "") {
+            toast.error("Please Enter Title")
+        } else if (formData.description == "") {
+            toast.error("Please Enter Description")
+        }
+        if (formData.title && formData.description) {
+            console.log("Idea Data: ", formData)
+        }
+    }
+
     return (
         <section section className='w-full h-full'>
             <div className='p-2 md:p-6 flex flex-col items-center justify-center gap-2 md:gap-4 w-full mx-auto md:w-3/4'>
                 <h1 className='text-2xl  md:text-3xl font-bold'>Edit Idea</h1>
-                <InputField
-                    label="Title"
-                    name="title"
-                    placeholder={"Title"}
-                    type="text" />
+                <form className='flex flex-col items-center justify-center gap-2 md:gap-4 w-full' onSubmit={handleSubmit}>
+                    <InputField
+                        label="Title"
+                        name="title"
+                        placeholder={"Title"}
+                        onChange={handleChange}
+                        type="text" />
 
-                <TextArea
-                    label="Description"
-                    name="description"
-                    placeholder={"Description"}
-                    type="text"
-                />
+                    <TextArea
+                        label="Description"
+                        name="description"
+                        placeholder={"Description"}
+                        onChange={handleChange}
+                        type="text"
+                    />
+                    <button className='w-full bg-primary text-white font-medium rounded p-2 '>Save</button>
+                </form>
 
-                <button className='w-full bg-primary text-white font-medium rounded p-2 '>Save</button>
 
             </div>
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputField } from '../components'
 import { Link } from 'react-router-dom'
+import { toast } from "react-hot-toast"
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (formData.email == "") {
+            toast.error("Please Enter email")
+        }
+        else if (formData.password == "") {
+            toast.error("Please Enter password")
+        }
+        if (formData.email && formData.password) {
+            console.log("Login Data: ", formData)
+        }
+
+    }
+
     return (
         <section className='w-full h-full'>
             <div className='w-full  md:w-2/4 mx-auto my-8  p-2 md:p-6  rounded flex flex-col items-center justify-center gap-6 md:shadow-lg bg-white md:border md:border-gray-600'>
                 <h1 className='text-2xl  md:text-3xl font-bold'>Login</h1>
-                <form className='w-full flex flex-col gap-6' >
+                <form className='w-full flex flex-col gap-6' onSubmit={handleSubmit}>
                     <InputField
                         label="Email"
                         name="email"
                         placeholder={"example@gmail.com"}
+                        onChange={handleChange}
                         type="email" />
                     <InputField
                         label="Password"
                         name="password"
                         placeholder={"**********"}
+                        onChange={handleChange}
                         type="password" />
 
                     <button className='w-full bg-primary text-white font-medium rounded p-2'>Login</button>
