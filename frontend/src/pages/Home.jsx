01ae70019c9a4cb0c86abe323a3bbf20d1d8a93e
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-09-09 18:09:33 +0530

    fetching blogs from backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IdeaCard } from '../components'
-import ideas from '../data.json'
+import axios from "axios"
 
 const Home = () => {
+    const [loading, setLoading] = useState(false)
+    const [ideas, setIdeas] = useState([])
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchIdeas = async () => {
+            try {
+                setLoading(true)
+                const response = await axios.get("/api/v1/idea/display")
+                setIdeas(response?.data?.ideas || [])
+                setLoading(false)
+            } catch (error) {
+                console.error("Error in fetching ideas:", error)
+                setError("Something went wrong while fetching ideas. Please try again.")
+                setLoading(false)
+            }
+        }
+        fetchIdeas()
+    }, [])
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>{error}</h1>
+    }
+
     return (
         <section className='w-full h-full p-2 md:p-6'>
-            <IdeaCard ideas={ideas} />
+            {ideas.length === 0 ? (
+                <h1>No Ideas to display</h1>
+            ) : (
+                <IdeaCard ideas={ideas} />
+            )}
         </section>
     )
 }
