ce730de993805e03af144da49c04ad624bfec6fd
kamal-deep-131 <kamaldeepsinghsodhi2@gmail.com>
2024-09-17 19:36:15 +0530

    removed unused functions

--- frontend/src/pages/SingleIdea.jsx
+++ frontend/src/pages/SingleIdea.jsx
@@ -2,18 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
-// Helper function to get user initials
-const getInitials = (name) => {
-    return name ? name.charAt(0).toUpperCase() : 'U'; // Default 'U' for unknown
-};
-
-// Helper function to format the date from MongoDB timestamp
-
-
-
 
 const SingleIdea = () => {
-    const { id } = useParams(); // Get the idea ID from the route parameters
+    const { id } = useParams();
     const [idea, setIdea] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -40,9 +31,8 @@ const SingleIdea = () => {
     return (
         <section className="max-w-4xl mx-auto p-4 md:py-12">
             <div className="bg-white shadow-md rounded-lg overflow-hidden">
-                {/* Header with gradient */}
 
-                {/* Content Area */}
+
                 <div className="p-8 flex flex-col gap-4">
                     <h1 className="text-4xl font-bold text-gray-900 p-4 border-l-8 border-primary bg-[#ffddb8]">{idea.title || 'Untitled Idea'}</h1>
                     <p className='text-black  text-sm font-semibold italic rounded'>
@@ -53,23 +43,6 @@ const SingleIdea = () => {
                         {idea.description || 'No description available.'}
                     </p>
 
-
-
-                    {/* Tags */}
-                    {idea.tags && idea.tags.length > 0 && (
-                        <div className="flex flex-wrap gap-2">
-                            {idea.tags.map((tag, index) => (
-                                <span
-                                    key={index}
-                                    className="bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-sm"
-                                >
-                                    #{tag}
-                                </span>
-                            ))}
-                        </div>
-                    )}
-
-                    {/* Back to all ideas button */}
                     <div >
                         <Link
                             to="/"
